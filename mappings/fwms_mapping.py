069dd25cb88799350196946d356efdd9c684c460
neemiasgabriel <neemias.arruda@picpay.com>
2024-03-01 12:42:32 -0300

    Update fwms_mapping.py

--- mappings/fwms_mapping.py
+++ mappings/fwms_mapping.py
@@ -8,84 +8,78 @@ from projects_resources import fernanda_project_acronym, other_projects_acronym,
 
 load_dotenv()
 
-def generate_csv(file_name, csv_matrix):
-  rows = ["{},{},{}".format(i, j, k) for i, j, k in csv_matrix]
-  text = "\n".join(rows)
 
-  with open(f'files/{file_name}.csv', 'w') as f:
-    f.write(text)
+def generate_csv(file_name, csv_matrix):
+    rows = ["{},{},{}".format(i, j, k) for i, j, k in csv_matrix]
+    text = "\n".join(rows)
+
+    with open(f'files/{file_name}.csv', 'w') as f:
+        f.write(text)
+
 
 def gitlab_api():
-  session = requests.Session()
-  return gitlab.Gitlab('https://git.original.com.br', private_token=os.getenv('GITLAB_TOKEN_COLLINS'), session=session)
+    session = requests.Session()
+    return gitlab.Gitlab('https://git.original.com.br',
+                         private_token=os.getenv('GITLAB_TOKEN_COLLINS'),
+                         session=session)
+
 
 def search_project_files(acronym_list):
-  csv_matrix = []
-  header = ["Sigla", "Nome da Variavel","URL"]
-  csv_matrix.append(header)
+    csv_matrix = []
+    header = ["Sigla", "Nome da variavel", "URL"]
+    csv_matrix.append(header)
 
-  regex = r"(?P<name>.*?)\s*=\s*(https?://)(?P<url_text>.*)"
-  gl = gitlab_api()
+    regex = r"(?P<name>.*?)\s*=\s*(https?://)(?P<url_text>.*)"
+    gl = gitlab_api()
 
-  for acronym in acronym_list:
-    fwms_regex = f"{acronym}-integration.properties"
+    project = gl.projects.get("fwms/fwms-configuracao-aplicacoes", all=True)
 
-    projects_list = gl.projects.list(search="fwms", all=True)
+    repo_files = project.repository_tree(ref='master', recursive=True, all=True)
 
-    for project in projects_list:
-      if not project.name.endswith('-java'):
-        continue
+    file_filter = []
 
-      try:
-        repo_files = project.repository_tree(ref='master', recursive=True, all=True)
-      except gitlab.exceptions.GitlabGetError as e:
-        if e.error_message == '404 Tree Not Found':
-          print(f"Project {project.name} has no folder structure")
-          repo_files = None
-
-      file_filter = []
-
-      if repo_files is None:
-        continue
-
-      for file in repo_files:
+    for file in repo_files:
         file_name = file.get('name')
 
-        if file_name.endswith('.gitignore') or not file_name.endswith('-corp.properties'):
-          continue
+        has_acronym = False
 
-        file_acronym = file_name.replace('-integration.properties', '')
+        for acronyms in acronym_list:
 
-        if file_acronym in (fernanda_project_acronym):
-          continue
+            if file_name.startswith(acronyms):
+                has_acronym = True
+                break
 
-        # if file_name in relevant_files or re.match(feign_regex, file.get('path')):
+        if has_acronym:
+            continue
 
-        if re.match(fwms_regex, file.get('path')):
-          file_info = {'name': file_name, 'path': file.get('path')}
-          file_filter.append(file_info)
+        if '-corp' in file_name or '.gitignore' in file_name:
+            continue
 
-      if len(file_filter) == 0:
-        continue
+        file_info = {'name': file_name, 'path': file.get('path')}
+        file_filter.append(file_info)
 
-      for filtered_file in file_filter:
+    for filtered_file in file_filter:
         path = filtered_file.get('path')
         file_content = project.files.get(ref='master', file_path=path)
         file_data = base64.b64decode(file_content.content).decode("utf-8").replace('\\n', '\n')
 
         for line in file_data.splitlines():
-          match = re.search(regex, line)
+            match = re.search(regex, line)
 
-          if match:
-            variable_name = match.group("name")
-            url_text = match.group("url_text")
-            csv_matrix.append([acronym, variable_name, url_text])
-            print(f"Acronym: {acronym} | Nome da Variável: {variable_name} | URL: {url_text}\n")
-  return csv_matrix
+            if match:
+                variable_name = match.group("name")
+                url_text = match.group("url_text")
+                acronym = filtered_file.get('name').replace('-integration.properties', '')
+                csv_matrix.append([acronym, variable_name, url_text])
+                print(f"Acronym: {acronym} | Nome da Variável: {variable_name} | URL: {url_text}\n")
+
+    return csv_matrix
+
 
 def main():
-  csv_matrix = search_project_files(fernanda_project_acronym)
-  generate_csv('fernanda_fwms_variables', csv_matrix)
+    csv_matrix = search_project_files(fernanda_project_acronym)
+    generate_csv('others_fwms_variables', csv_matrix)
+
 
 if __name__ == '__main__':
-  main()
+    main()
