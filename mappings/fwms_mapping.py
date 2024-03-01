afb1f0ebcf10a51bd95151fd0b5fe1c019b4c707
neemiasgabriel <neemias.arruda@picpay.com>
2024-03-01 15:07:27 -0300

    update fwms representation

--- mappings/fwms_mapping.py
+++ mappings/fwms_mapping.py
@@ -1,5 +1,6 @@
 import os
 import re
+import json
 import base64
 import gitlab
 import requests
@@ -26,8 +27,9 @@ def gitlab_api():
 
 def search_project_files(acronym_list):
     csv_matrix = []
+    dictionary = {}
     header = ["Sigla", "Nome da variavel", "URL"]
-    csv_matrix.append(header)
+    # csv_matrix.append(header)
 
     regex = r"(?P<name>.*?)\s*=\s*(https?://)(?P<url_text>.*)"
     gl = gitlab_api()
@@ -36,24 +38,24 @@ def search_project_files(acronym_list):
 
     repo_files = project.repository_tree(ref='master', recursive=True, all=True)
 
+    if repo_files is None:
+        return None
+
     file_filter = []
+
 
     for file in repo_files:
         file_name = file.get('name')
 
-        has_acronym = False
-
-        for acronyms in acronym_list:
-
-            if file_name.startswith(acronyms):
-                has_acronym = True
-                break
-
-        if has_acronym:
+        if '-corp.properties' in file_name or '.gitignore' in file_name:
             continue
 
-        if '-corp' in file_name or '.gitignore' in file_name:
-            continue
+        check_name = file_name.replace('-integration.properties', '')
+
+        # if check_name in acronym_list:
+        #     continue
+
+        dictionary[check_name] = []
 
         file_info = {'name': file_name, 'path': file.get('path')}
         file_filter.append(file_info)
@@ -70,16 +72,21 @@ def search_project_files(acronym_list):
                 variable_name = match.group("name")
                 url_text = match.group("url_text")
                 acronym = filtered_file.get('name').replace('-integration.properties', '')
-                csv_matrix.append([acronym, variable_name, url_text])
-                print(f"Acronym: {acronym} | Nome da Variável: {variable_name} | URL: {url_text}\n")
+                dictionary[acronym].append({'project_name': variable_name, 'url': url_text})
+                # csv_matrix.append([acronym, variable_name, url_text])
+                # print(f"Acronym: {acronym} | Nome da Variável: {variable_name} | URL: {url_text}\n")
 
-    return csv_matrix
+    return csv_matrix, dictionary
 
 
 def main():
-    csv_matrix = search_project_files(fernanda_project_acronym)
-    generate_csv('others_fwms_variables', csv_matrix)
-
+    csv_matrix, dictionary = search_project_files(fernanda_project_acronym)
+    # generate_csv('others_fwms_variables', csv_matrix)
+    with open('../files/fwms_dictionary.json', 'w+') as f:
+        try:
+            json.dump(dictionary, f)
+        except json.decoder.JSONDecodeError:
+            print(dictionary)
 
 if __name__ == '__main__':
     main()
